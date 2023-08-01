3f8964976b628143c516db8d92e3b8ff9e7119f5
Youssef <youssef@ctd.ma>
2023-08-01 07:52:00 -0400

    refactoring

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,23 @@
-import { appDomain, wildcard } from "./utils/constants";
+import { appDomain, DEFAULT_LOCAL_WILDCARD_DOMAIN } from "@/utils/constants";
 import { NextResponse } from "next/server";
 import { parse } from "tldts";
+
+/**
+ * Checks if the given pathname is an exception.
+ * @param {string} pathname - The URL pathname.
+ * @returns {boolean} - True if the pathname is an exception, false otherwise.
+ */
 const isException = (pathname) => {
   return /^\/(_next|api|_static|locales|fonts|images)/.test(pathname);
 };
 
+/**
+ * Middleware function to handle URL rewriting and redirection.
+ * @param {object} req - The request object.
+ * @returns {NextResponse | Response | void} - The response object or void if no action is needed.
+ */
 export function middleware(req) {
-  //get the pathname and the locale from the url (the pathname is with a slash at the begenning)
+  // Get the pathname and the locale from the URL (the pathname has a slash at the beginning)
   const { pathname, locale } = req.nextUrl;
   const isDev = process.env.NODE_ENV === "development";
 
@@ -14,7 +25,7 @@ export function middleware(req) {
     return;
   }
 
-  //get the host name
+  // Get the host name
   let host = req.headers.get("host");
 
   // If localhost, assign the host value manually
@@ -23,7 +34,6 @@ export function middleware(req) {
     ? host?.replace(`.localhost:3000`, "")
     : parse(host).subdomain;
 
-  console.log(currentHost);
   // Prevent security issues – users should not be able to canonically access
   // the pages/sites folder and its respective contents. This can also be done
   // via rewrites to a custom 404 page
@@ -31,23 +41,19 @@ export function middleware(req) {
     return new Response(null, { status: 404 });
   }
 
-  //if the host name is the main/app host (landing page to enter the business infos),
-  //or there is not a host name rewrite to the appdomain route (create)
+  // If the host name is the main/app host "create",
+  // or there is not a host name, rewrite to the app domain route (create)
   if (
     currentHost === appDomain ||
-    (isDev ? currentHost === "localhost:3000" : currentHost === wildcard)
+    (isDev && currentHost === DEFAULT_LOCAL_WILDCARD_DOMAIN)
   ) {
-    const newUrl = new URL(req.url);
-    // console.log("================================");
-    // console.log("pathname", newUrl.toString());
-    // console.log("================================");
     return NextResponse.rewrite(
       new URL(`${locale}/${appDomain}${pathname}`, req.url)
     );
   }
 
-  // rewrite to the current hostname under the pages/_sites folder
-  // the main logic component will happen in pages/_sites/[site]/index.tsx
+  // Rewrite to the current hostname under the pages/_sites folder
+  // The main logic component will happen in pages/_sites/[site]/index.tsx
   return NextResponse.rewrite(
     new URL(`${locale}/_sites/${currentHost}${pathname}`, req.url)
   );
